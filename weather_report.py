ee350f83a6262c1a0b0d37209d75f33df2eb7de0
Ginno <ginnocode@gmail.com>
2024-09-04 14:32:58 -0400

    Se refactorizo weather_report.py

--- weather_report.py
+++ weather_report.py
@@ -1,25 +1,31 @@
-import requests
 import argparse
+import requests
+from api_key import API_KEY
 
-api_key = "d3e70d5e32b4d965bf35a75ae7faa919"
+def get_weather(location, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
-parser = argparse.ArgumentParser(description="Obtener el clima para una ubicación específica.")
-parser.add_argument("--location", type=str, required=True, help="Nombre de la ciudad y país (por ejemplo, 'Lima')")
-parser.add_argument("--format", type=str, choices=["json", "csv", "text"], default="text", help="Formato de salida (json, csv, text)")
-
-args = parser.parse_args()
-
-locationn = args.location
-format = args.format
-url = f"http://api.openweathermap.org/data/2.5/weather?q={locationn}&appid={api_key}&units=metric" 
-
-data = requests.get(url)
-weather_data = data.json()
-if weather_data:
-    if format == "json":
-        print(weather_data.json())
-    elif format == "text":
-        print(f"El clima en {locationn}: {weather_data['weather'][0]['description']}, {weather_data['main']['temp']}°C")
+def main():
+    parser = argparse.ArgumentParser(description="Obtener el clima para una ubicación específica.")
+    parser.add_argument("--location", type=str, required=True, help="Nombre de la ciudad y país (por ejemplo, 'Lima')")
+    parser.add_argument("--format", type=str, choices=["json", "csv", "text"], default="text", help="Formato de salida (json, csv, text)")
     
-else:
-    print("No se pudo obtener el clima. Verifica la ubicación o tu conexión.")
+    args = parser.parse_args()
+
+    api_key = API_KEY
+    weather_data = get_weather(args.location, api_key)
+    if weather_data:
+        if args.format == "json":
+            print(weather_data)
+        elif args.format == "text":
+            print(f"El clima en {args.location}: {weather_data['weather'][0]['description']}, {weather_data['main']['temp']}°C")
+    else:
+        print("No se pudo obtener el clima. Verifica la ubicación o tu conexión.")
+
+if __name__ == "__main__":
+    main()
