87f24cea0990f9dde4242e2b3c14c227079b3830
Ginno <ginnocode@gmail.com>
2024-09-04 14:19:37 -0400

    Se agrego la funcion de argumentos CLI

--- weather_report.py
+++ weather_report.py
@@ -1,17 +1,25 @@
 import requests
+import argparse
 
-location = "Asuncion"
 api_key = "d3e70d5e32b4d965bf35a75ae7faa919"
-format = "json"
 
-url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric" 
+parser = argparse.ArgumentParser(description="Obtener el clima para una ubicación específica.")
+parser.add_argument("--location", type=str, required=True, help="Nombre de la ciudad y país (por ejemplo, 'Lima')")
+parser.add_argument("--format", type=str, choices=["json", "csv", "text"], default="text", help="Formato de salida (json, csv, text)")
 
-weather_data = requests.get(url)
+args = parser.parse_args()
+
+locationn = args.location
+format = args.format
+url = f"http://api.openweathermap.org/data/2.5/weather?q={locationn}&appid={api_key}&units=metric" 
+
+data = requests.get(url)
+weather_data = data.json()
 if weather_data:
     if format == "json":
         print(weather_data.json())
     elif format == "text":
-        print(f"El clima en {location}: {weather_data['weather'][0]['description']}, {weather_data['main']['temp']}°C")
-    # Puedes agregar la lógica para csv aquí
+        print(f"El clima en {locationn}: {weather_data['weather'][0]['description']}, {weather_data['main']['temp']}°C")
+    
 else:
     print("No se pudo obtener el clima. Verifica la ubicación o tu conexión.")
